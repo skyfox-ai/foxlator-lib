159b8d9384e809424cf16683edbd57a96d82b6f0
nekoffski <kacperczyz@protonmail.com>
2023-10-18 14:47:40 +0200

    Initial commit

---
+++ bin/build-package.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3.11 -m pip install --upgrade build
+
+python3.11 -m build
---
+++ bin/install-deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update
+sudo apt-get install -y python3.11 python3.11-distutils python3.11-venv
