a306b2531a5cfe34583eb68711d89d2b3e809a43
nekoffski <kacperczyz@protonmail.com>
2023-10-19 22:24:33 +0200

    Create req file for build requirements

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -8,5 +8,4 @@ sudo apt-get install -y python3.10 python3.10-distutils python3.10-venv
 echo "$ - installing pip"
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 
-python3.10 -m pip install --upgrade build
-python3.10 -m pip install --upgrade twine
+python3.10 -m pip install --upgrade -r ./build-requirements.txt
