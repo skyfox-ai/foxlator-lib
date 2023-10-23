41af4d21e98b3f696574205cf938d35d3ad139ca
nekoffski <kacperczyz@protonmail.com>
2023-10-23 17:15:44 +0200

    Set up a test runner

---
+++ test/basic_tests.py
@@ -0,0 +1,7 @@
+from utils import base
+
+
+class BasicTests(base.TestBase):
+    def test_dummy_test_placeholder(self):
+        """ Dummy test to verify if test pipeline works """
+        self.assertTrue(True)
---
+++ test/utils/base.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+# any utility functions go here
+class TestBase(unittest.TestCase):
+    pass
