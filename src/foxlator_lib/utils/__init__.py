816ca2fc801cc993442d2d13f8a90d9aadaccd48
nekoffski <kacperczyz@protonmail.com>
2023-10-25 22:05:05 +0200

    Move utils to another directory

--- src/foxlator_lib/utils/__init__.py
+++ src/foxlator_lib/utils/__init__.py
@@ -1,9 +1,7 @@
 
+from .file_system import FileSystem
+
+
 def get_version() -> str:
     import importlib.metadata
     return importlib.metadata.version('foxlator_lib')
-
-
-class FileSystem(object):
-    def is_file(self) -> bool:
-        pass
