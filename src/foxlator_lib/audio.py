63258e11cd73bfdc75728c8e0442eff7e65765a7
nekoffski <kacperczyz@protonmail.com>
2023-11-02 23:15:48 +0100

    Implement audio extracting

--- src/foxlator_lib/audio.py
+++ src/foxlator_lib/audio.py
@@ -1,5 +1,11 @@
 
+import moviepy.editor as mp  # type: ignore
+
 
 class AudioPath(object):
+    def __init__(self, audio: mp.AudioFileClip):
+        self.audio = audio
+
     def save(self, path: str):
-        pass
+        # TODO: consider taking codec/extensions as parameter instead of using .mp3
+        self.audio.write_audiofile(path + '.mp3', logger=None)
--- src/foxlator_lib/video.py
+++ src/foxlator_lib/video.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import moviepy.editor as mp  # type: ignore
 
 from .audio import AudioPath
 from .utils import FileSystem
@@ -21,9 +22,12 @@ class Video(object):
 
         if (not self.fs.is_file(path)):
             raise VideoError(f"Video file: '{path}' doesn't exist")
+        self.clip: mp.VideoFileClip = mp.VideoFileClip(self.path)
 
     def get_audio_path(self) -> AudioPath:
-        return AudioPath()
+        if self.clip.audio:
+            return AudioPath(audio=self.clip.audio)
+        raise VideoError(reason="Clip doesn't have audio path")
 
     def _validate_extension(self, path: str):
         extension: str = pathlib.Path(path).suffix
