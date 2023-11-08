fbc7e636e1038485579f6ef57407c05ad5db4fc8
Kacper Czyż <kacperczyz@protonmail.com>
2023-11-08 08:07:58 +0100

    Add function for applying subtitles on a clip (#7)

--- src/foxlator_lib/video.py
+++ src/foxlator_lib/video.py
@@ -1,10 +1,23 @@
 import pathlib
+import dataclasses
+import typing
 
 import moviepy.editor as mp  # type: ignore
+from moviepy.video.tools.subtitles import SubtitlesClip  # type: ignore
 
 from .audio import AudioPath
 from .utils import FileSystem
 from .error import BaseError
+
+
+@dataclasses.dataclass
+class SubtitleFrame:
+    begin: int
+    end: int
+    text: str
+
+    def serialize(self):
+        return ((self.begin, self.end), self.text)
 
 
 class VideoError(BaseError):
@@ -17,22 +30,35 @@ class Video(object):
     def __init__(self, path: str, fs: FileSystem = FileSystem()):
         self.path = path
         self.fs = fs
-
-        self._validate_extension(path)
+        self.extension = self._validate_extension(path)
 
         if (not self.fs.is_file(path)):
             raise VideoError(f"Video file: '{path}' doesn't exist")
         self.clip: mp.VideoFileClip = mp.VideoFileClip(self.path)
+
+    def apply_subtitles(self, subtitles: typing.List[SubtitleFrame], path: str):
+        # TODO: make it all configurable (in next version?), research if there is any better way of
+        # TODO: editing frames with timestamps (preferably compatible with opencv)
+        def generator(text: str) -> mp.TextClip:
+            return mp.TextClip(
+                text, font='Arial', fontsize=24, color='white')
+        subtitles_clip = SubtitlesClip(
+            [subtitle.serialize() for subtitle in subtitles], generator)
+        composed_clip = mp.CompositeVideoClip([
+            self.clip, subtitles_clip.set_position(('center', 'bottom'))
+        ])
+        composed_clip.write_videofile(path + self.extension, logger=None)
 
     def get_audio_path(self) -> AudioPath:
         if self.clip.audio:
             return AudioPath(audio=self.clip.audio)
         raise VideoError(reason="Clip doesn't have audio path")
 
-    def _validate_extension(self, path: str):
+    def _validate_extension(self, path: str) -> str:
         extension: str = pathlib.Path(path).suffix
         if len(extension) == 0:
             raise VideoError(
                 "Provided file path is invalid or doesn't contain extension")
         if extension not in self.known_extensions:
             raise VideoError(f"Unsupported file extension: {extension}")
+        return extension
