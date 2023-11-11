54c155419e3b212dab26eb15ec96a04fcde3c04f
nekoffski <kacperczyz@protonmail.com>
2023-11-11 14:21:40 +0100

    Add ffmpeg to deps

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -3,7 +3,7 @@
 echo "$ - installing python interpreter and necessary modules"
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt-get install -y python3.10 python3.10-distutils python3.10-venv
+sudo apt-get install -y python3.10 python3.10-distutils python3.10-venv ffmpeg
 
 echo "$ - installing pip"
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
