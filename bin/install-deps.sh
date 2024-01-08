5c436d9ac1f213c03cc3f2d1967edce62b7a6d56
hazka <witekhaface@gmail.com>
2024-01-08 19:08:15 +0100

    Fix for ImageMagick in install-deps.sh part 11

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -5,18 +5,10 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 sudo apt-get install -y python3.10 python3.10-distutils python3.10-venv ffmpeg
 
-echo "$ - fixing ImageMagick detection bug"
-mkdir -p /tmp/distr && \
-    cd /tmp/distr && \
-    wget https://download.imagemagick.org/ImageMagick/download/releases/ImageMagick-7.0.11-2.tar.xz && \
-    tar xvf ImageMagick-7.0.11-2.tar.xz && \
-    cd ImageMagick-7.0.11-2 && \
-    ./configure --enable-shared=yes --disable-static --without-perl && \
-    make && \
-    make install && \
-    ldconfig /usr/local/lib && \
-    cd /tmp && \
-    rm -rf distr
+echo "$ - Configure ImageMagick policy"
+sudo cat /etc/ImageMagick-6/policy.xml \
+          | sed 's/none/read,write/g' \
+          | sudo tee /etc/ImageMagick-6/policy.xml
 
 echo "$ - installing pip"
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
