68dc1f2ab1f671c57f51f87184f295ecd0fa78d8
hazka <witekhaface@gmail.com>
2024-01-08 20:13:29 +0100

    Fix for ImageMagick in install-deps.sh part 19

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -5,7 +5,6 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 sudo apt remove imagemagick* -y
 sudo apt-get install -y python3.10 python3.10-distutils python3.10-venv ffmpeg imagemagick
-python3.10 -m install moviepy[optional]
 sudo sed -i '/<policy domain="path" rights="none" pattern="@\*"/d' /etc/ImageMagick-6/policy.xml
 
 echo "$ - installing pip"
