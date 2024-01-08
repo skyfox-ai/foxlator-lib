3afee292c9b84683761335c63f6dec2f48f43d59
hazka <witekhaface@gmail.com>
2024-01-08 19:37:02 +0100

    Fix for ImageMagick in install-deps.sh part 13

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -3,15 +3,9 @@
 echo "$ - installing python interpreter and necessary modules"
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
+sudo apt-get install -y imagemagick
+sudo sed -i '/<policy domain="path" rights="none" pattern="@\*"/d' /etc/ImageMagick-6/policy.xml
 sudo apt-get install -y python3.10 python3.10-distutils python3.10-venv ffmpeg imagemagick
-
-echo "$ - Checking ImageMagick" 
-command -v convert >/dev/null 2>&1 || { echo >&2 "ImageMagick is not installed"; exit 1; }
-
-echo "$ - Configure ImageMagick policy"
-sudo cat /etc/ImageMagick-6/policy.xml \
-          | sed 's/none/read,write/g' \
-          | sudo tee /etc/ImageMagick-6/policy.xml
 
 echo "$ - installing pip"
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
