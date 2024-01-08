139cfffff5d6f14a7855696a307b280eefcf07d9
hazka <witekhaface@gmail.com>
2024-01-08 19:46:58 +0100

    Fix for ImageMagick in install-deps.sh part 15

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -6,10 +6,10 @@ sudo apt-get update
 sudo apt-get install -y imagemagick
 
 sudo apt-get install -y python3.10 python3.10-distutils python3.10-venv ffmpeg
-python3.10 -m install moviepy[optional]
-sudo sed -i '/<policy domain="path" rights="none" pattern="@\*"/d' /etc/ImageMagick-6/policy.xml
 
 echo "$ - installing pip"
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 
 python3.10 -m pip install --upgrade -r ./build-requirements.txt
+python3.10 -m install moviepy[optional]
+sudo sed -i '/<policy domain="path" rights="none" pattern="@\*"/d' /etc/ImageMagick-6/policy.xml
