58c87733999c77e35603011344ac3d2defd01242
hazka <witekhaface@gmail.com>
2024-01-08 14:35:36 +0100

    Deleted dead comments.

--- test/utils/base.py
+++ test/utils/base.py
@@ -4,12 +4,3 @@ import unittest
 
 class TestBase(unittest.TestCase):
     pass
-
-#     def create_test_video_file(self):
-# frame = np.zeros((480, 640, 3), dtype=np.uint8)
-# frame.fill(255)
-# video_clip = VideoClip(frame, duration=2)
-# with tempfile.NamedTemporaryFile(suffix=".mp4") as temp_file:
-#     temp_path = temp_file.name
-#     video_clip.write_videofile(temp_path, codec="libx264", fps=25)
-#     return temp_path
